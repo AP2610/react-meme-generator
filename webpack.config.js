36976dbcb6bc85495c97c18fb3b0610679cccd63
Arjun Puri <arjun_261094@hotmail.com>
2020-01-06 14:08:33 +0100

    reconfigured webpack, added babel, started project

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,43 @@
+const webpack = require('webpack');
 const path = require('path');
 
 const config = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    'css-loader'
-                ]
-            }
+  entry: [
+    'react-hot-loader/patch',
+    './src/index.js'
+  ],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
         ]
+      },
+      {
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [
+      '.js',
+      '.jsx'
+    ],
+    alias: {
+      'react-dom': '@hot-loader/react-dom'
     }
+  },
+  devServer: {
+    contentBase: './dist'
+  }
 };
 
-module.exports = config
+module.exports = config;
